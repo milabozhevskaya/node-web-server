1d37a8e31451d8c3ae1f86e143ffba4c306be42c
Mila Bozhevskaya <mi.bozhev@gmail.com>
2022-02-13 08:59:28 +0200

    feat: add session logger

--- serve/logger.js
+++ serve/logger.js
@@ -30,3 +30,8 @@ function parseLog(text) {
   }));
   return records;
 }
+
+function logSession(session) {
+  const record = new Date().toLocaleString('en-CA', { hourCycle: 'h24' }) + '\tServer starts...\n';
+  fs.promises.appendFile('./logs/server.log', record);
+}
