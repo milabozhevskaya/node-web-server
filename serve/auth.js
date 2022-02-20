034c737538438da4c33ed532eb4066209bbfc5dc
Mila Bozhevskaya <mi.bozhev@gmail.com>
2022-02-20 09:57:13 +0200

    fix: fix bug, narrow down try scope

--- serve/auth.js
+++ serve/auth.js
@@ -6,19 +6,20 @@ const { hash } = require('./helpers/crypto');
 const { getBody } = require('./helpers/getBody');
 
 async function auth(request, response) {
+  let body;
   try {
-    const body = await getBody(request);
-    const user = getUsers().find((user) => user.username === body.username);
-    if (!user || !await hash.verify(body.password, user.pwdHash)) return { success: false };
-    const token = genToken();
-    const start = Date.now();
-    sessions.push({id: genID(), userId: user.id, token, start, expire: start + 1 * 60e3});
-    storeAuthTS({ [user.username]: start });
-    response.setHeader('set-cookie', 'nws_info_token=' + token);
-    return { success: true, token };
+    body = await getBody(request);
   } catch (error) {
     return { errors: ['JSON data only'] };
   }
+  const user = getUsers().find((user) => user.username === body.username);
+  if (!user || !await hash.verify(body.password, user.pwdHash)) return { success: false };
+  const token = genToken();
+  const start = Date.now();
+  sessions.push({ id: genID(), userId: user.id, token, start, expire: start + 1 * 60e3 });
+  storeAuthTS({ [user.username]: start });
+  response.setHeader('set-cookie', 'nws_info_token=' + token);
+  return { success: true, token };
 }
 
 module.exports = { auth };
